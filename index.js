531c5f13aa16bfd4c00d1aaa56577edf44e98064
thcccloud <thcccloud@hotmail.com>
2025-04-10 19:36:19 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-const PORT = 3000;
+const PORT = 8080;
 
 const server = http.createServer((req, res) => {
   if (req.url === '/' || req.url === '/index.html') {
